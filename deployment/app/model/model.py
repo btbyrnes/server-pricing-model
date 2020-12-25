43070d57e989ec78cc5a8490d0843bfb06a78034
btbyrnes <61630357+btbyrnes@users.noreply.github.com>
2020-12-25 17:51:32 -0500

    Update model.py

--- deployment/app/model/model.py
+++ deployment/app/model/model.py
@@ -1,19 +1,20 @@
 import numpy as np
 from sklearn import linear_model
 
-import os
-
 import pickle
 import os
 
 model_file = 'model/model.pkl'
 
+# Load the model from the pickle file
 with open(model_file,'rb') as f:
     model_saved = pickle.load(f)
 
+# Take the strings of the features and return the prediction
 def predict_handler(x1,x2,x3,x4):
     x = np.array([float(x1),float(x2),float(x3),float(x4)]).reshape(1,-1)
     return predict(x).item(0)
 
+# Take an nparray of length 4 and return the model
 def predict(X):
     return model_saved.predict(X)
